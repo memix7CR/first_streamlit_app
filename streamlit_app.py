174341b983d0fc7a04c32d36bfb66b03beb92b85
memix7CR <40298909+memix7CR@users.noreply.github.com>
2022-11-01 15:23:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie')
