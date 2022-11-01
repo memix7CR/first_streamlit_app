54253a998cd17e06b33d432b6914387a7dc0932c
memix7CR <40298909+memix7CR@users.noreply.github.com>
2022-11-01 15:03:33 -0600

    Update streamlit_app.py
    More additions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('Another brick in the wall')
+streamlit.title('My Parents New Healthty Dinner')
+streamlit.header('Breakfast Menu'
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
